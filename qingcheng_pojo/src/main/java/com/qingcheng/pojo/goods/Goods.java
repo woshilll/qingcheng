91ecf1cf72f7f1eff1ff303f9407996290ed01c9
liyang <15238376457@163.com>
2019-10-14 15:07:52 +0800

    有关青橙项目的第一次提交

---
+++ qingcheng_pojo/src/main/java/com/qingcheng/pojo/goods/Goods.java
@@ -0,0 +1,29 @@
+package com.qingcheng.pojo.goods;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author 李洋
+ * @date 2019-10-01 17:02
+ */
+public class Goods implements Serializable {
+    private Spu spu;
+    private List<Sku> skuList;
+
+    public Spu getSpu() {
+        return spu;
+    }
+
+    public void setSpu(Spu spu) {
+        this.spu = spu;
+    }
+
+    public List<Sku> getSkuList() {
+        return skuList;
+    }
+
+    public void setSkuList(List<Sku> skuList) {
+        this.skuList = skuList;
+    }
+}
---
+++ qingcheng_service_goods/src/main/java/com/qingcheng/dao/CategoryBrandMapper.java
@@ -0,0 +1,11 @@
+package com.qingcheng.dao;
+
+import com.qingcheng.pojo.goods.CategoryBrand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author 李洋
+ * @date 2019-10-14 14:09
+ */
+public interface CategoryBrandMapper extends Mapper<CategoryBrand> {
+}
